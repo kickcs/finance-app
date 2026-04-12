98d247253d6a63bbf0eb9b2c70f94d26fb4542eb
aabdusamatov <aabdusamatov@hamkorlab.club>
2026-04-12 12:45:53 +0500

    chore(add-transaction): export useDebtForm from feature index

--- frontend/src/features/add-transaction/index.ts
+++ frontend/src/features/add-transaction/index.ts
@@ -1,4 +1,6 @@
 export { default as TransactionForm } from './ui/TransactionForm.vue';
 export { useTransactionForm } from './model/useTransactionForm';
 export { useSubmitTransaction } from './model/useSubmitTransaction';
+export { useDebtForm } from './model/useDebtForm';
 export type { TransactionFormData } from './model/useTransactionForm';
+export type { DebtFormData } from './model/useDebtForm';
